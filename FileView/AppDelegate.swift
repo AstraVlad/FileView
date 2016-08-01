cf47faa43158d1969a975f82b2708511a483aca9
Vladimir Feldman <wfel@narod.ru>
2016-08-01 14:08:16 +0300

    Initial Commit

---
+++ FileView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FileView
+//
+//  Created by Vladimir Feldman on 01.08.16.
+//  Copyright © 2016 Vladimir Feldman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FileView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileView
+//
+//  Created by Vladimir Feldman on 01.08.16.
+//  Copyright © 2016 Vladimir Feldman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
